14832001ab91830231a915082d5531e8ba5f05f4
cj633931 <cj633931@southeast.edu>
2020-02-17 14:51:40 -0600

    Stuff

--- MathWiz/Models/Assignment.cs
+++ MathWiz/Models/Assignment.cs
@@ -7,38 +7,5 @@ namespace MathWiz.Models
 {
     public class Assignment
     {
-        public List<Problem> Problems { get; set; }
-        public string ID { get; set; }
-        public string Name { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
-        public DateTime? DueBy { get; set; }
-        public int Type { get; set; }
-
-        public Assignment() { }
-
-        public Assignment(string name, int type=1, DateTime? dueBy=null)
-        {
-            this.Name = name;
-            this.Type = type;
-            this.DueBy = dueBy;
-            // Database.AddAssignment(name,type,dueBy);
-        }
-
-        public decimal Grade()
-        {
-            int problemCount = this.Problems.Count();
-            int correctProblems = 0;
-            decimal percentage = 0.00m;
-            foreach (Problem problem in this.Problems)
-            {
-                if (problem.correct)
-                {
-                    correctProblems++;
-                }
-            }
-            percentage = ((decimal)correctProblems / (decimal)problemCount);
-            return percentage;
-        }
     }
 }
