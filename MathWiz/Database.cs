86fe5e23222548281642218b14642070d362d834
Luther Jensen <stinkeebow63@gmail.com>
2020-02-17 13:43:52 -0600

    Added in database

---
+++ MathWiz/Database.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MathWiz
+{
+    public class Database
+    {
+
+        private static SqlConnection _db;
+
+        static Database()
+        {
+            string connectionString = ConfigurationManager.AppSettings["DatabaseConnection"];
+            _db = new SqlConnection(connectionString);
+        }
+
+        public static SqlConnection getInstance()
+        {
+            if(_db == null)
+            {
+                new Database();
+            }
+            return _db;
+        }
+
+    }
+}
