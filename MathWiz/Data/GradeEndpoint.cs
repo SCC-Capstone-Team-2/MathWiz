0e539eed9a039755814f67da146a1c1ba873ea73
Luther Jensen <stinkeebow63@gmail.com>
2020-02-19 13:35:37 -0600

    Added in the grade view model and endoint. Did more work on the student endpoint and model.

---
+++ MathWiz/Data/GradeEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace MathWiz.Data
+{
+    public class GradeEndpoint
+    {
+
+        public static bool AddGradeForAssignment(int AssignmentID, int StudentID, Decimal grade)
+        {
+            string query = "INSERT INTO Grade VALUES (@AssignmentID, @StudentID, @Grade)";
+            using(SqlConnection conn = Database.getInstance())
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@AssignmentID", AssignmentID);
+                cmd.Parameters.AddWithValue("@StudentID", StudentID);
+                cmd.Parameters.AddWithValue("@Grade", grade);
+                int rowsAffected = cmd.ExecuteNonQuery();
+                return rowsAffected.Equals(1);
+            }
+        }
+
+    }
+}
